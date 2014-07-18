ad5af402a2048ac8fab1131b65380739fbaab68e
David Sin <davidsin1990@gmail.com>
2014-07-18 16:24:49 -0400

    finish decks

--- app/controllers/deck.rb
+++ app/controllers/deck.rb
@@ -1,10 +1,27 @@
-get '/deck/:id' do 
+#shows all decks
+get '/decks' do
+  @decks = Deck.all
+
+  erb :'deck/index'
 end
 
+# show specific deck
+get '/deck/:id' do
+  @deck = Deck.find(params[:id])
+
+  erb :'deck/deck'
+end
+
+#goes to create new deck form
 get '/deck/new' do
+
+  erb :'deck/new'
 end
 
+#posts the new deck to database
 post '/deck' do
+  @deck = Deck.create(params[:deck])
+  redirect '/'
 end
 
 
