1580054e9cf3a9f7fb59394b818ad9a53cd4a323
Farheen Malik <farheen.m.malik@gmail.com>
2014-07-18 14:11:35 -0400

    Add bcrypt methods to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,26 @@
 class User < ActiveRecord::Base
 	has_many :rounds
 	has_many :decks
+
+	include BCrypt #a module
+
+  def password
+    @password ||= Password.new(password_hash)
+  end
+
+  def password=(new_password)
+    @password = Password.create(new_password)
+    self.password_hash = @password
+  end
+
+  def self.authenticate(email,password)
+  	user = User.find_by_email(email)
+  	if user && user.password == password
+  		user
+  	else
+  		nil
+  	end
+  end
+  
   # Remember to create a migration!
 end
