a334f7ca22cd48ac4075495db6e1577a12334156
Farheen Malik <fma2@github.com>
2014-07-18 16:17:33 -0400

    Add helper, modify user controller to allow for authenticated login

--- app/controllers/user.rb
+++ app/controllers/user.rb
@@ -1,5 +1,28 @@
 post '/user/new' do
+  @user = User.create(params[:signup])
+  redirect '/decks'
 end
 
 post '/user' do
+  user = User.authenticate(params[:login][:email], params[:login][:password])
+  if user
+    session[:user_id] = user.id #params[:user_id]
+    redirect to "/decks"
+  else
+    @errors = "couldn't log you in"
+    redirect to '/'
+  end
 end
+
+get '/user/logout' do
+  session[:user_id] = nil
+  # session.save
+  session.clear
+  redirect to "/"
+
+end
+get '/decks'
+  erb :'deck/index.erb'
+end
+
+
