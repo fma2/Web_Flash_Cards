3a09462accf27936e8217837c098283812a6722b
Farheen Malik <farheen.m.malik@gmail.com>
2014-07-18 12:26:10 -0400

    Add migration file

---
+++ db/migrate/20140718121955_create_tables.rb
@@ -0,0 +1,37 @@
+class CreateTables < ActiveRecord::Migration
+  def change
+  	create_table :users do |t|
+  		t.string :name
+  		t.string :email
+  		t.string :password
+  		t.timestamps
+  	end
+
+  	create_table :rounds do |t|
+  		t.references :user
+  		t.timestamps
+  	end
+  	
+  	create_table :guesses do |t|
+  		t.references :round
+  		t.references :card
+  		t.boolean :correct, :default => false
+  	end
+  	
+  	create_table :cards do |t|
+  		t.references :deck
+  		t.string :question
+  		t.string :choice1
+  		t.string :choice2
+  		t.string :choice3
+  		t.string :answer
+  		t.timestamps
+  	end
+  	
+  	create_table :decks do |t|
+  		t.string :name
+  		t.references :user
+  	end
+  	
+  end
+end
